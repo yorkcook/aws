1ef2beb0253068b617bc094b2dca6e28edfa42a4
York Cook <yorkcook@Johnny-Castle.lan1>
2019-09-19 13:00:41 -0400

    WIP: server running

---
+++ knexfile.js
@@ -0,0 +1,57 @@
+// Update with your config settings.
+
+const localPg = {
+  host: "localhost",
+  database: "users",
+  user: "York",
+  password: "baller"
+};
+
+const productionDbConnection = process.env.DATABASE_URL || localPg;
+
+module.exports = {
+  development: {
+    client: "sqlite3",
+    connection: {
+      filename: "./database/recipe.db3"
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./database/migrations"
+    },
+    seeds: {
+      directory: "./database/seeds"
+    }
+  },
+
+  // staging: {
+  //   client: "postgresql",
+  //   connection: {
+  //     database: "my_db",
+  //     user: "username",
+  //     password: "password"
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: "knex_migrations"
+  //   }
+  // },
+
+  production: {
+    client: "pg",
+    connection: productionDbConnection,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: "./database/migrations"
+    },
+    seeds: {
+      directory: "./database/seeds"
+    }
+  }
+};
